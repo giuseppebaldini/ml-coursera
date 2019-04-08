7b79b23947aaba159b5a267d92b7da09553ffcca
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-04-08 22:49:08 +0800

    Initial commit

---
+++ Week 2/Octave_Tutorial.m
@@ -0,0 +1,23 @@
+% Octave basics
+
+1+2
+
+c = 'Hello World!'
+c
+
+1 == 2 % false
+
+1~= 2 % true
+
+1 && 2 % and
+
+1 || 2 % or
+
+PS1('>> '); 
+
+a = pi
+
+% Vectors & Matrices
+
+A = [1 2; 3 4; 5 6]
+
