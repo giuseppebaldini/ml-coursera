eb34ff63a411023505a76d2eb54010e717596e67
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-04-10 13:09:52 +0800

    Notes formatting

--- Week 2/Octave_Tutorials/2.Moving_Data.m
+++ Week 2/Octave_Tutorials/2.Moving_Data.m
@@ -2,5 +2,22 @@
 
 A = [1 2; 3 4; 5 6]
 
-size(A)
+size(A) % returns 1 x 2 matrix with size
+size(A,1) % returns first dimension (rows)
+length(A) % size of longest dimension
 
+pwd % print working dir
+cd % change dir
+ls % list files in dir
+
+load('filename.txt')
+
+who % shows variables (top row)
+whos % detailed view on the data
+
+save file.txt v -ascii 
+
+A(2,3) % returns variable A - row 2 - col 3
+A([1 2], :) % row 1 and 2 - all cols
+A = [A, [5; 42; 99]] % append a new col
+A(:) % put all elements of A in a vector
