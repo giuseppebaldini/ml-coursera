24f7771959cf49c33220840a3dcd54234b09dedb
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-04-13 12:21:13 +0800

    Initial commit

--- Week 2/Octave_Tutorials/5.Control_Statements.m
+++ Week 2/Octave_Tutorials/5.Control_Statements.m
@@ -1,3 +1,19 @@
 % Control Statements
 
 v = zeros(10,1)
+
+for i = 1:10,
+  v(i) = 2^i;
+endfor
+
+v
+
+v(1) = 42
+
+if v(1) == 1,
+  disp('The value is one');
+elseif v(1) == 42;
+  disp('The answer to everything')
+else
+  disp('We have no idea what is the value');
+endif
