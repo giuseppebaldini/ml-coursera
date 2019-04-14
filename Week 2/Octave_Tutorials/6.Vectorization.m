9dbed774842ff9ea9e3b5e3538736680ecafc7ae
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-04-14 16:22:16 +0800

    Initial commit

--- Week 2/Octave_Tutorials/6.Vectorization.m
+++ Week 2/Octave_Tutorials/6.Vectorization.m
@@ -1,2 +1,10 @@
 % Vectorization
 
+% unvectorized implementation
+prediction = 0.0;
+for j = 1:n+1,
+  .prediction = prediction + theta(j) * x(j)
+endfor
+
+% vectorized implementation (optimized lin alg routine)
+prediction = theta' * x;
