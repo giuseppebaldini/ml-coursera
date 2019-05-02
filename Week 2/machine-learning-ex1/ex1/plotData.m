4f4cf932598377884412c210920ebfb56f9250ea
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-05-02 19:46:46 +0200

    Initial commit

--- Week 2/machine-learning-ex1/ex1/plotData.m
+++ Week 2/machine-learning-ex1/ex1/plotData.m
@@ -5,6 +5,10 @@ function plotData(x, y)
 
 figure; % open a new figure window
 
+plot(x, y, 'rx', 'MarkerSize', 10); % Plot the data
+ylabel('Profit in $10,000s'); % Set the y?axis label
+xlabel('Population of City in 10,000s'); % Set the x?axis label
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Plot the training data into a figure using the 
 %               "figure" and "plot" commands. Set the axes labels using
