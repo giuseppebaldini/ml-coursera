4abea89f14b1e21f75d352df9c792f1fedf7f783
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-05-04 14:23:00 +0200

    Initial commit

--- Week 2/machine-learning-ex1/ex1/gradientDescent.m
+++ Week 2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,11 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
+    % using the Normal Equation
+    
+    theta = pinv(X' * X) * X' * y;
 
 
     % ============================================================
