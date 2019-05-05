6c19ac711362357a3a3e5b13d9a1fc5ee234759a
GiuseppeBaldini <40420789+GiuseppeBaldini@users.noreply.github.com>
2019-05-05 20:15:37 +0200

    Initial commit

--- Week 2/machine-learning-ex1/ex1/gradientDescent.m
+++ Week 2/machine-learning-ex1/ex1/gradientDescent.m
@@ -16,6 +16,7 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     % 
+    % NOTE: Supports multiple variables
     
     h = X*theta;
     delta = (1/m) * X' * (h - y);
